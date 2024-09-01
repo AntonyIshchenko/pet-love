5b97c5afbfb4845fb644dee4b9c791a1c701c239
Antony Ishchenko <antony.ishchenko@gmail.com>
2024-09-01 10:01:44 +0300

    styles-setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: { sourcemap: true },
+  resolve: {
+    alias: {
+      components: '/src/components',
+      pages: '/src/pages',
+      styles: '/src/styles',
+      fonts: '/src/fonts',
+      // apiService: '/src/apiService',
+      // hooks: '/src/hooks',
+    },
+  },
+});
